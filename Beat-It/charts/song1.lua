76f5e353ae8f5c0006e8d91043bf3cb3abad22a1
Ashlee J <56657815+Ashteroide@users.noreply.github.com>
2021-06-02 22:25:48 +1000

    Introduced Song1
    * No current song
    * Will be used for further testing

---
+++ Beat-It/charts/song1.lua
@@ -0,0 +1,12 @@
+return
+{
+	speed = 1,
+	{
+        sectionNotes =
+        {
+            { noteTime = 0, noteType = 2, noteLength = 0 },
+            { noteTime = 600, noteType = 3, noteLength = 450 },
+            { noteTime = 1050, noteType = 3, noteLength = 600 }
+        }
+    },
+}
