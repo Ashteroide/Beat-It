9a286ebaa85e3ff3576621dd050c7871364e0401
Pharap <2933055+Pharap@users.noreply.github.com>
2021-06-06 01:32:07 +0100

    Improve keybind system
    This took a little bit more thought than I originally expected, but I think this arrangement conveys how a keybind table works very well.
    It might be possible to generate the 'block handlers' in a for loop, but it might not be as easy as anticipated due to 'closures', so that will need a bit more investigation.

--- Beat-It/game.lua
+++ Beat-It/game.lua
@@ -1,9 +1,51 @@
 game =
 {
     load = function(self)
-        -- Key Pattern: Left, Down, Up, Right
-        self.keys = { false, false, false, false, false, false, false, false };
-        self.printKeys = { "a", "s", "w", "d", "left", "down", "up", "right" };
+        -- Represent whether blocks have been enabled
+        self.blocks = { false, false, false, false };
+
+        -- Key handlers for activating and deactivating blocks
+        local block1Handler =
+        {
+            press = function(self) self.blocks[1] = true; end,
+            release = function(self) self.blocks[1] = false; end,
+        };
+        local block2Handler =
+        {
+            press = function(self) self.blocks[2] = true; end,
+            release = function(self) self.blocks[2] = false; end,
+        };
+        local block3Handler =
+        {
+            press = function(self) self.blocks[3] = true; end,
+            release = function(self) self.blocks[3] = false; end,
+        };
+        local block4Handler =
+        {
+            press = function(self) self.blocks[4] = true; end,
+            release = function(self) self.blocks[4] = false; end,
+        };
+
+        -- Function handler for leaving the state
+        local escapeHandler =
+        {
+            press = function(self) currentState = "Menu"; end,
+            release = function(self) end,
+        };
+
+        -- Keybind table
+        self.keybinds =
+        {
+            ["a"] = block1Handler,
+            ["w"] = block2Handler,
+            ["s"] = block3Handler,
+            ["d"] = block4Handler,
+            ["left"] = block1Handler,
+            ["up"] = block2Handler,
+            ["down"] = block3Handler,
+            ["right"] = block4Handler,
+            ["escape"] = escapeHandler,
+        };
     end,
 
     update = function(self)
@@ -20,29 +62,23 @@ game =
     draw = function(self)
         love.graphics.print("Game Mode!");
 
-        for index = 1, #self.keys do
-            if self.keys[index] == true then
-                love.graphics.setColor(1, 1, 1);
-                love.graphics.print(self.printKeys[index] .. "!", 0, (index * 12));
-            end
-        end
-
         local windowWidth = love.graphics.getWidth();
         local windowHeight = love.graphics.getHeight();
 
-        love.graphics.translate( Xposition, (windowHeight - hitBoxHeight - hitBoxClearance) );
+        love.graphics.push();
+        love.graphics.translate(Xposition, (windowHeight - hitBoxHeight - hitBoxClearance) );
 
-        for index = 1, 4, 1 do
-            if self.keys[index] == true or self.keys[index + 4] == true then
+        for index = 1, #self.blocks do
+            if self.blocks[index] then
                 love.graphics.setColor(0, 1, 0);
-                love.graphics.rectangle("fill", (((hitBoxWidth + hitBoxClearance) * index) - hitBoxWidth), 0, hitBoxWidth, hitBoxHeight);
             else
                 love.graphics.setColor(1, 0, 0);
-                love.graphics.rectangle("fill", (((hitBoxWidth + hitBoxClearance) * index) - hitBoxWidth), 0, hitBoxWidth, hitBoxHeight);
             end
+
+            love.graphics.rectangle("fill", (((hitBoxWidth + hitBoxClearance) * index) - hitBoxWidth), 0, hitBoxWidth, hitBoxHeight);
         end
 
-        love.graphics.translate(-Xposition, -(windowHeight - hitBoxHeight - hitBoxClearance));
+        love.graphics.pop();
 
         love.graphics.setColor(1, 1, 1);
         love.graphics.rectangle("fill", (Xposition - 2), 0, hitBoxClearance, windowHeight);
@@ -52,22 +88,18 @@ game =
     end,
 
     keyPressed = function(self, key)
-        if key == "escape" then
-            currentState = "Menu";
-        end
+        local handler = self.keybinds[key];
 
-        for index = 1, #self.keys do
-            if key == self.printKeys[index] then
-                self.keys[index] = true;
-            end
+        if handler ~= nil then
+            handler.press(self);
         end
     end,
 
     keyReleased = function(self, key)
-        for index = 1, #self.keys do
-            if key == self.printKeys[index] then
-                self.keys[index] = false;
-            end
+        local handler = self.keybinds[key];
+
+        if handler ~= nil then
+            handler.release(self);
         end
     end
 }
