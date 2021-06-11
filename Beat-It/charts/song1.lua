5953b6ce9705bafaad8ee46ceccb8cefb22cece8
Ashlee J <56657815+Ashteroide@users.noreply.github.com>
2021-06-11 20:42:52 +1000

    Adjusted song1.lua * Added `tempo = 120` * Adjusted `sectionNotes` * Made basic notes for testing later on

--- Beat-It/charts/song1.lua
+++ Beat-It/charts/song1.lua
@@ -1,12 +1,12 @@
-return
+song1 = 
 {
-	speed = 1,
-	{
-        sectionNotes =
-        {
-            { noteTime = 0, noteType = 2, noteLength = 0 },
-            { noteTime = 600, noteType = 3, noteLength = 450 },
-            { noteTime = 1050, noteType = 3, noteLength = 600 }
-        }
-    },
+    tempo = 120,
+    
+    sectionNotes =
+    {
+        { noteTime = 0, noteType = 1, noteLength = 0 },
+        { noteTime = 240, noteType = 2, noteLength = 0 },
+        { noteTime = 360, noteType = 3, noteLength = 0 },
+        { noteTime = 480, noteType = 4, noteLength = 0 }
+    }
 }
